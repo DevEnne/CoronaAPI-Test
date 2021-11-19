4ee5e1302f84263d6c1e14874f5349b502aefbad
KimJei <marks_0_bears@icloud.com>
2021-11-19 12:25:03 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 $key = $_POST['apikey'];
-if ( ! empty( $key ) ) :
-    $data_str = file_get_contents('https://api.corona-19.kr/korea/?serviceKey='. $key);
-    $json = json_decode($data_str, true);
-endif;
-?>
 
+if (!empty($key)) {
+  $existKey = true;
+} else {
+  $existKey = false;
+}
+
+if ($existKey) {
+  $data = @file_get_contents('https://raw.githubusercontent.com/dhlife09/Corona-19-API/master/3_beta.json?serviceKey='. $key);
+  $json = json_decode($data, true);
+}
+?>
 
 <!DOCTYPE html>
 <html lang="kr">
@@ -17,27 +23,45 @@ endif;
 <form method="POST" action="index.php">
         API KEY: <input type="text" name="apikey"/><br/>
         <input type="submit" name="submit"/>
-</form>
+<form>
 <table class="type02">
   <tr>
     <th scope="row">API Key</th>
-    <td><?php echo $key ?></td>
+    <td><?php if($existKey) { echo $key; } ?>
+</td>
   </tr>
   <tr>
     <th scope="row">상태</th>
-    <td><?php
-if ( ! empty( $key ) ) {
-  echo $json['resultMessage'];
-} else {
-  $resultMessage = "API키를 입력해주세요.";
-  echo $resultMessage;
-}
-?></td>
+    <td>
+    <?php if($existKey) { echo $json['API']['resultMessage']; } else { echo 'API 키를 입력해주세요.'; } ?>
+</td>
   </tr>
   <tr>
     <th scope="row">업데이트 시간</th>
-    <td><?php echo $json['updateTime']; ?></td>
+    <td><?php if($existKey) { echo $json['API']['updateTime']; } ?></td>
+  </tr>
+  <tr>
+    <th scope="row">국내 확진자 수</th>
+    <td><?php if($existKey) { echo number_format($json['korea']['totalCnt']). "명"; } ?></td>
+  </tr>
+  <tr>
+    <th scope="row">국내 전일대비 확진자 수</th>
+    <td><?php if($existKey) { echo '+'. number_format($json['korea']['incDec']). "명"; } ?></td>
+  </tr>
+  <tr>
+    <th scope="row">국내 완치자 수</th>
+    <td><?php if($existKey) { echo number_format($json['korea']['recCnt']). "명"; } ?></td>
+  </tr>
+  <tr>
+    <th scope="row">국내 사망자 수</th>
+    <td><?php if($existKey) { echo number_format($json['korea']['deathCnt']). "명"; } ?></td>
+  </tr>
+  <tr>
+    <th scope="row">국내 치료중 환자 수</th>
+    <td><?php if($existKey) { echo number_format($json['korea']['isolCnt']). "명"; } ?></td>
+  </tr>
+  <tr>
+    <th scope="row">국내 코로나 발생률</th>
+    <td><?php if($existKey) { echo $json['korea']['qurRate']. "%"; } ?></td>
   </tr>
 </table>
-
-
